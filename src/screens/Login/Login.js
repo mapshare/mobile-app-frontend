2741edcc65a44fd64ad945fe8000a68407b9acbd
Wahab Zafar <Wzafar@arraymarketing.com>
2019-10-31 16:39:19 -0400

    commit

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -20,7 +20,7 @@ class LogIn extends Component {
                 <ImageBackground resizeMode = "cover" style = {styles.backgroundImage} source = {require('../../assests/images/logo.png')}>
                         <Text style={styles.title}>Welcome to Pin IT!{"\n"}{"\n"}</Text>
                         <Text style={styles.text}>(Please note that this is WIP apk)</Text>
-                        <Text style={styles.text}>Current Build v1.02</Text>
+                        <Text style={styles.text}>Current Build v1.06</Text>
                         <LogInForm type="Login" style={styles.container}/>
                         <View>
                             <Text style={styles.signupText}>
